fb4f9a4de9c154d62ba350afba2eca47a1f37304
Carterfmeng <mf654428310@gmail.com>
2022-04-20 21:08:12 +0800

    change the error message for the proj1 EC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -12,36 +12,31 @@ public class TestArrayDequeEC {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
         int N = 500;
+        String errorMessage = "";
         for (int i = 0; i < N; i++) {
             /* generate random number from [0, 5). */
             int randomOperationNum = StdRandom.uniform(5);
             if (randomOperationNum == 0) {
                 sad1.addFirst(i);
                 ads1.addFirst(i);
-                System.out.println("sad1 ==> addFirst(" + i + ")");
-                System.out.println("ads1 ==> addFirst(" + i + ")");
-                assertEquals("deque size not equal", sad1.size(), ads1.size());
+                errorMessage += "addFirst(" + i + ")\n";
             } else if (randomOperationNum == 1) {
                 sad1.addLast(i);
                 ads1.addLast(i);
-                System.out.println("sad1 ==> addLast(" + i + ")");
-                System.out.println("ads1 ==> addLast(" + i + ")");
-                assertEquals("deque size not equal", sad1.size(), ads1.size());
+                errorMessage += "addLast(" + i + ")\n";
             } else if (randomOperationNum == 2) {
                 if (sad1.size() > 0) {
                     int removedValueStudent = sad1.removeFirst();
                     int removedValueSolution = ads1.removeFirst();
-                    System.out.println("sad1 ==> removeFirst(" + removedValueStudent + ")");
-                    System.out.println("ads1 ==> removeFirst(" + removedValueSolution + ")");
-                    assertEquals("remove value not the same", removedValueStudent, removedValueSolution);
+                    errorMessage += "removeFirst()\n";
+                    assertEquals(errorMessage, removedValueStudent, removedValueSolution);
                 }
             } else if (randomOperationNum == 3) {
                 if (sad1.size() > 0) {
                     int removedValueStudent = sad1.removeLast();
                     int removedValueSolution = ads1.removeLast();
-                    System.out.println("sad1 ==> removeLast(" + removedValueStudent + ")");
-                    System.out.println("ads1 ==> removeLast(" + removedValueSolution + ")");
-                    assertEquals("remove value not the same", removedValueStudent, removedValueSolution);
+                    errorMessage += "removeLast()\n";
+                    assertEquals(errorMessage, removedValueStudent, removedValueSolution);
                 }
             }
         }

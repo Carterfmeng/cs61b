b469fdcb70c5cc49469f382886d845bacb9528fb
Carterfmeng <mf654428310@gmail.com>
2022-07-28 09:06:58 +0800

    finish lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -19,29 +19,61 @@ public class HexWorld {
     private static final long SEED = 23333;
     private static final Random RANDOM = new Random(SEED);
 
-    public static void addHexagon(TETile[][] world, int s, int xCor, int yCor, TETile t) {
-        drawRectangle(world, s, xCor, yCor, t);
-        int leftTriangleXCor = xCor - 1;
-        int leftTriangleYCor = yCor + 1;
-        int rightTriangleXCor = xCor + s;
-        int rightTriangleYCor = yCor + 1;
-        drawTriangle(world, s, leftTriangleXCor, leftTriangleYCor, t, -1);
-        drawTriangle(world, s, rightTriangleXCor, rightTriangleYCor, t, 1);
+    private static class Position {
+        private int x;
+        private int y;
+
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        Position shiftPosition(int dx, int dy) {
+            return new Position(this.x + dx, this.y + dy);
+        }
     }
 
-    private static void drawRectangle(TETile[][] world, int s, int xCor, int yCor, TETile t) {
-        for (int x = xCor; x < xCor + s; x += 1) {
-            for (int y = yCor; y < yCor + s * 2; y += 1) {
+    private static void addHexagonWithRandomTile(TETile[][] world, int s, Position p) {
+        int tileNum = RANDOM.nextInt(3);
+        TETile t = Tileset.NOTHING;
+        switch (tileNum) {
+            case 0:
+                t = Tileset.GRASS;
+                break;
+            case 1:
+                t = Tileset.FLOWER;
+                break;
+            case 2:
+                t = Tileset.WATER;
+                break;
+
+        }
+        addHexagon(world, s, p, t);
+    }
+
+    public static void addHexagon(TETile[][] world, int s, Position p, TETile t) {
+        drawRectangle(world, s, p, t);
+        Position leftTrianglePosition = p.shiftPosition(-1, 1);
+        Position rightTrianglePosition = p.shiftPosition(s, 1);
+        drawTriangle(world, s, leftTrianglePosition, t, -1);
+        drawTriangle(world, s, rightTrianglePosition, t, 1);
+    }
+
+    private static void drawRectangle(TETile[][] world, int s, Position p, TETile t) {
+        for (int x = p.x; x < p.x + s; x += 1) {
+            for (int y = p.y; y < p.y + s * 2; y += 1) {
                 world[x][y] = t;
             }
         }
     }
 
-    private static void drawTriangle(TETile[][] world, int s, int xTCor, int yTCor, TETile t, int direction) {
-        int x = xTCor;
+    private static void drawTriangle(TETile[][] world, int s, Position p, TETile t, int direction) {
+        int px = p.x;
+        int py = p.y;
+        int x = px;
         for (int offSet = 0; Math.abs(offSet) < s; offSet += direction) {
-            for (int y = yTCor + Math.abs(offSet); y < yTCor + s * 2 - 2 - Math.abs(offSet); y += 1) {
-                x = xTCor + offSet;
+            for (int y = py + Math.abs(offSet); y < py + s * 2 - 2 - Math.abs(offSet); y += 1) {
+                x = px + offSet;
                 world[x][y] = t;
             }
         }
@@ -58,13 +90,17 @@ public class HexWorld {
         }
     }
 
+
     public static void main(String[] args) {
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
 
         TETile[][] world = new TETile[WIDTH][HEIGHT];
         fillWorldNull(world);
-        addHexagon(world, 5, 10, 10, Tileset.FLOWER);
+        Position p1 = new Position(10, 10);
+        Position p2 = new Position(20, 20);
+        addHexagonWithRandomTile(world, 3, p1);
+        addHexagonWithRandomTile(world, 3, p2);
         ter.renderFrame(world);
     }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,34 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
+Answer: I tried to draw the tessellation from the top hexagon, or from the central hexagon, and my start position for a hexagon is from the first pixel without Nothing tile.
+
+the same problem can use different direction, like from up to down is difficult to draw the word, but left to right is easy, sometime you just need to tweak a little about your thought.
+
+don't neglect any complexity, like firstly I don't abstract the position, it makes difficult to figure out the new position and do the shift operation.
 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
-Answer:
+Answer: rooms and hallways are different hexagons, and tesselation is to generate the random world with these rooms and hallways;
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer: generate random size room, generate random size hallway
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
 Answer:
+distinguishes: 
+1. hallway's width is limited to 1 or 2 tiles, and it's shape must be straight.
+2. hallways can have turns, but rooms can't have turns.
+
+similarity:
+1. they both consist of walls and tiles.
+2. they both reachable.
+3. the world the consist together must be closed.
